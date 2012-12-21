46511549be10f91580cb9106c881545a67939983
Roman Heinrich <roman.heinrich@gmail.com>
2012-12-21 19:03:42 +0200

    password for user model

--- _templates/pagination/app/models/user.rb
+++ _templates/pagination/app/models/user.rb
@@ -2,11 +2,17 @@ ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "db/
 class User < ActiveRecord::Base
   col :username
   col :email
+  col :password
   timestamps
+
+  def self.authenticate(login, password)
+    where(:login => login, :password => password).first
+  end
+
 end
 User.auto_upgrade!
 unless User.first
   100.times do |i|
-    User.create(:username => "User_#{i}", :email => "email-#{i}@example.com")
+    User.create(:username => "user_#{i}", :email => "email-#{i}@example.com", :password => 'test')
   end
 end
